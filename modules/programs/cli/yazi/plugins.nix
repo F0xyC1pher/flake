0f164f5ec2d62d184445ea8eff79c373f96e014b
FoxyChipher <ageev-eldar@mail.ru>
2026-08-31 12:56:00 +0300

    yazi WiP

--- modules/programs/cli/yazi/plugins.nix
+++ modules/programs/cli/yazi/plugins.nix
@@ -8,20 +8,19 @@
 		users.${vars.user.name} = {pkgs, ...}: {
 			programs.yazi = {
 				plugins = with pkgs.yaziPlugins; {
-					sudo = sudo;
-					glow = glow;
-					gvfs = gvfs;
-					piper = piper;
-					mount = mount;
-					gitui = gitui;
-					chmod = chmod;
-					miller = miller;
-					yatline = yatline;
-					mime-ext = mime-ext;
-					compress = compress;
-					mediainfo = mediainfo;
-					toggle-pane = toggle-pane;
-					full-border = full-border;
+					# sudo = sudo;
+					# glow = glow;
+					# gvfs = gvfs;
+					# piper = piper;
+					# mount = mount;
+					# gitui = gitui;
+					# chmod = chmod;
+					# yatline = yatline;
+					# mime-ext = mime-ext;
+					# compress = compress;
+					# mediainfo = mediainfo;
+					# toggle-pane = toggle-pane;
+					# full-border = full-border;
 				};
 			};
 		};
