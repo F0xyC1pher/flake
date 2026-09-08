44a87ee215e91ed770a1657b219136fa7d5d9435
F0xyC1pher <ageev-eldar@mail.ru>
2026-09-08 17:31:00 +0300

    WonderLand to wonderland (kebab-case) hosts/wonderland/hardware-configuration.nix fix also stateVersion fix and meta.nix update

--- hosts/WonderLand/default.nix
+++
@@ -1,6 +0,0 @@
-{...}: {
-	imports = [
-		./hardware-configuration.nix
-	];
-	system.stateVersion = "26.11"; #place your number upon installing
-}
---
+++ hosts/wonderland/default.nix
@@ -0,0 +1,6 @@
+{...}: {
+	imports = [
+		./hardware-configuration.nix
+	];
+	system.stateVersion = "XX.YY"; #place your number upon installing
+}
