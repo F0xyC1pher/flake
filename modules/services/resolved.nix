df212a0b38ec6f6aef2edf9ace64fdf884868daf
F0xyC1pher <ageev-eldar@mail.ru>
2026-09-09 00:20:05 +0300

    9.9.9.9

--- modules/services/resolved.nix
+++ modules/services/resolved.nix
@@ -1,29 +1,28 @@
-{ ... }:
-{
-  services.resolved = {
-    enable = true;
+{...}: {
+	services.resolved = {
+		enable = true;
 
-    settings = {
-      Resolve = {
-        # DNS = ["127.0.0.1:5300"];
-        DNS = [
-          "1.1.1.1#one.one.one.one"
-          "1.0.0.1#one.one.one.one"
-        ];
-        FallbackDNS = [
-          "1.1.1.1#one.one.one.one"
-          "1.0.0.1#one.one.one.one"
-        ];
-        Domains = [ "~." ];
-        DNSStubListener = false;
-        # dnssec = "true";
-        # dnsovertls = "true";
-        # Add this to prevent fallback to per-link DNS
-        ReadEtcHosts = true;
-        # Игнорируем DNS, которые прилетают от VPN-ссылок
-        # LLMNR = false;
-        # MulticastDNS = false;
-      };
-    };
-  };
+		settings = {
+			Resolve = {
+				# DNS = ["127.0.0.1:5300"];
+				DNS = [
+					"9.9.9.9"
+					# "1.0.0.1#one.one.one.one"
+				];
+				# FallbackDNS = [
+				#   "1.1.1.1#one.one.one.one"
+				#   "1.0.0.1#one.one.one.one"
+				# ];
+				Domains = ["~."];
+				DNSStubListener = false;
+				# dnssec = "true";
+				# dnsovertls = "true";
+				# Add this to prevent fallback to per-link DNS
+				ReadEtcHosts = true;
+				# Игнорируем DNS, которые прилетают от VPN-ссылок
+				# LLMNR = false;
+				# MulticastDNS = false;
+			};
+		};
+	};
 }
