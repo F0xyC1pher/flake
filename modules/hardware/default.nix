15d73d83f4149a672bb48b625779581936239676
F0xyC1pher <ageev-eldar@mail.ru>
2026-09-09 04:34:42 +0300

    i forgot to add .nix to bluetooth file in imports what an idiot i am

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -13,7 +13,7 @@ in {
 			./redist.nix
 			./ssd.nix
 		]
-		++ lib.optionals (vars.host.hardware.bluetooth.enable or false) [./bluetooth]
+		++ lib.optionals (vars.host.hardware.bluetooth.enable or false) [./bluetooth.nix]
 		++ lib.optionals (vars.host.parallels.enable or false) [./parallels.nix]
 		++ lib.optionals hasSwapCfg [./swap.nix];
 }
